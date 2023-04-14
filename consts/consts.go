20532f7b7fda5fe2829509a27d43548844115460
chengzhx76 <chengzhx76@qq.com>
2023-04-15 01:12:14 +0800

    update

--- consts/consts.go
+++ consts/consts.go
@@ -33,6 +33,7 @@ const (
 	DATE_FORMAT                     = "2006-01-02"
 	DATE_FORMAT_MONTH               = "2006-01"
 	DATE_FORMAT_YEAR                = "2006"
+	DATE_FORMAT_TIME                = "01-02 15:04"
 	TIME_FORMAT                     = "15:04:05"
 	DATE_FORMAT_HOUR_MINUTE         = "15:04"
 
